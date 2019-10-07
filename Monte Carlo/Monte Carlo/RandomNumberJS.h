15465cc0d7bf91f60b629fb7961d331230555705
Jerry <Jerry_Shen0527@outlook.com>
2019-10-07 23:17:35 +0800

    Sampling from an ugly function.

---
+++ Monte Carlo/Monte Carlo/RandomNumberJS.h
@@ -0,0 +1,13 @@
+#ifndef RANDOMNUMBERJS //DB is short for my name Jerry Shen
+#define RANDOMNUMBERJS
+namespace randomjs //防止日后调库的命名冲突
+{
+	double RandomSchrage();
+	double RandomFibonacci();
+	void SetSeed(int seed);
+}
+
+
+#endif // !RANDOMJS
+
+
